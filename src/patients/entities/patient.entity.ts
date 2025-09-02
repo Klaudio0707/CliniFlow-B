8df7bf442b06eaf453b7ad8290120d152530368f
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-09-02 20:23:18 -0300

    feat(patients): Add patient entity and creation DTO

--- src/patients/entities/patient.entity.ts
+++ src/patients/entities/patient.entity.ts
@@ -1,1 +1,34 @@
-export class Patient {}
+
+import { Column, DataType, HasMany, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+
+@Table({ tableName: 'patients', timestamps:true})
+
+export class PatientEntity extends Model {
+    @PrimaryKey
+    @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4, })
+    declare id: string;
+
+    @Column({ type: DataType.STRING, allowNull: false, })
+    declare name: string;
+
+    @Column({ type: DataType.STRING, allowNull: false,})
+    declare InsuranceCardNumber:string;
+    
+    @Column({type: DataType.STRING, allowNull: false,})
+    declare insurancePlanName:string;
+    
+    @Column({type: DataType.DATEONLY, allowNull: true,})
+    declare cardExpirationDate: Date | null;
+
+
+    @Column({type: DataType.STRING, allowNull: true,})
+    declare phone: string | null;
+
+    @Column({type: DataType.DATEONLY, allowNull: true,})
+    declare birthdate?: Date;
+
+    @HasMany(() => AppointmentEntity)
+    declare appointments: AppointmentEntity[];
+
+}
