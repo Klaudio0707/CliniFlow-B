a18a47e40d419cf10e8d3472fb232495172807ed
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-09-03 19:14:58 -0300

    feat(patients): Scaffold patients module

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { PatientsController } from './patients.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { PatientEntity } from './entities/patient.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([PatientEntity]),],
   controllers: [PatientsController],
   providers: [PatientsService],
+  exports: [PatientsService],
 })
 export class PatientsModule {}
